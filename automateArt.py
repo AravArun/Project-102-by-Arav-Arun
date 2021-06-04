17c531cd58bf5bc21c7c201492aa9433e547cf24
Arav Arun <72263186+AravArun@users.noreply.github.com>
2021-06-04 15:16:34 +0530

    first commit

---
+++ automateArt.py
@@ -0,0 +1,38 @@
+print("Five steps for a creating digital artwork")
+step = input("whats step number- ")
+if(step == '1'):
+    print("Step 1: Starting")
+    print("Every drawing starts somewhere.open your inspiration to draw the image or reference in your drawing software")
+
+elif(step == '2'):
+    print("Step 2: Lineing.")
+    print("Once you've got your ideas, start bordering or making a rough sketch of the drawing.After you have made the rough sketch start giving the final lines")
+
+elif(step == '3'):
+    print("Step 3: Inking")
+    print("Ok so now you are done drawing it and now its time to colour it.Scribble a bit of the colour you have decided to colour on the part you want to colour.Once you know which colour to put start colouring it.")
+
+elif(step == '4'):
+    print("Step 4: Detailing")
+    print("Now you are done with the basic part of it.Give the details now like shadow colors, proper lineing, customizing it.")
+
+elif(step == '5'):
+    print("Step 5: Background")
+    print("Now lets start coloring the background.Hide the layer of your drawings and only view the background layer.Draw the bacground and colour it or just use the gradient tool to give it a gradient background.")
+
+elif(step == '6'):
+    print("Step 6: saving and transfering")
+    print("Save your drawing as a jpeg file in your drawing images folder.Then using office transfer it to your phone or just mail it to your phone.")
+elif(step == '7'):
+    print("Step 7: Uploading to social media")
+    print("Ok now lets upload your drawings on all the art platforms, here is a list for guide :-")
+    print("1.Instagram")
+    print("2.Reddit")
+    print("3.GoodWall")
+    print("4.PenUp")
+    print("5.ArtStation")
+    print("6.DeviantArt")
+    print("7. Pinterest")
+    print("follow @artwitharav on instagram btw 😃")
+elif(step >= '8'):
+    print("Step 8: All done 👍")
